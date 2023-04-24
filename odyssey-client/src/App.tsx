268fce3864bd8011092eeb3b577d1ab683b7f1d9
qrlandry <qrlandry@gmail.com>
2023-04-24 17:25:24 -0400

    set up project in ts

--- odyssey-client/src/App.tsx
+++ odyssey-client/src/App.tsx
@@ -1,11 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello!</h1>
-      </header>
+      <h1> Odyssey </h1>
     </div>
   );
 }
