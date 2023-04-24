57c1706bdb48cf4d2ae86894106abb1afe48b8bd
qrlandry <qrlandry@gmail.com>
2023-04-24 18:08:13 -0400

    r

--- odyssey-client/src/services/client.ts
+++
@@ -1,8 +0,0 @@
-import axios, {AxiosInstance} from 'axios';
-
-const amadeus: AxiosInstance = axios.create({
-    baseURL: 'https://test.api.amadeus.com/v1',
-    headers: {
-      Authorization: `Bearer ${process.env.API_SECRET}`,
-    },
-});
