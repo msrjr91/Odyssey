be30e7539370c6e0ac75653936f68db4b264512e
Michael <michael.s.ross10@gmail.com>
2023-04-28 13:35:41 -0600

    recreated typescript and react project folder for client side using vite

--- odyssey-client/src/App.tsx
+++ odyssey-client/src/App.tsx
@@ -1,39 +1,35 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import "./App.css";
-
-const apiURL = "https://test.api.amadeus.com/v1";
-
-export type Cities = {
-  type: string;
-  subType: string;
-  name: string;
-  iataCode: string;
-  address: {
-    countryCode: string;
-  };
-  geoCode: {
-    latitude: number;
-    longitude: number;
-  };
-};
-
-function CityList({ cities }: { cities: Cities[] }) {
-  return (
-    <ul>
-      {cities.map((city) => (
-        <li key={city.iataCode}>{city.name}</li>
-      ))}
-    </ul>
-  );
-}
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-    </div>
-)}
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
